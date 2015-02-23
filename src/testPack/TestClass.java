811a17c9f7c7eae0f01f752f138cb886ca83a215
etrayneva <elenaot.et@gmail.com>
2015-02-23 21:26:04 +0300

    initial project

---
+++ src/testPack/TestClass.java
@@ -0,0 +1,14 @@
+package testPack;
+
+/**
+ * Created by elena on 23.02.15.
+ */
+public class TestClass {
+
+    public static void main(String args[]){
+
+        System.out.println("Hellow Intellij Idea!");
+
+    }
+
+}
